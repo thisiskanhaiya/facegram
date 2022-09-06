2d36040371df7cdc05df71b9a7da46917f4dbd1c
Kanhaiya <kgawade7878@gmail.com>
2022-09-06 15:15:02 +0530

    Added Home Page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import Navi from './NavigationBar/Navigationbar'
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './Home.css'
 function Home() {
   return (
-    <div>
-      <Navi/>
-    </div>
+    <Container>
+      <Row>
+        <Navi/>
+      </Row>
+      <Row>
+
+      
+          <div className='left-nav'>
+                left
+          </div>
+          <div className='right-nav'>
+                Right
+          </div>
+       
+      </Row>
+    </Container>
   )
 }
 
--- src/Components/NavigationBar/Navigationbar.jsx
+++ src/Components/NavigationBar/Navigationbar.jsx
@@ -16,7 +16,7 @@ function Navigationbar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#link">Add</Nav.Link>
           <Nav  className="justify-content-end" style={{ width: "100%" }}>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
